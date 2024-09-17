2fff21c77ddb0e8e80e50d0a6dd3b0c5a3031530
Miquel Sánchez-Osuna <miquelsanchezosuna@gmail.com>
2024-09-17 10:21:36 +0200

    Update wgs_summary.py

--- scripts/wgs_summary.py
+++ scripts/wgs_summary.py
@@ -7,120 +7,137 @@ import csv, json
 
 
 def mlst_data(filename):
-    # Open the file with the specified filename and read all lines
-    with open(filename) as f:
-        lines = f.readlines()
-    lines = [x.strip() for x in lines]
-    lineage = "Unknown"
-    # Iterate over each line in the file
-    for line in lines:
-        # Check if the line contains the string "Sequence Type: "
-        if "Sequence Type: " in line:
-            # If "unknown" is not in the line, extract the sequence type
-            if not "known" in line:
-                lineage = "ST"+line.split(" ")[-1]
-    # Return the determined lineage
-    return (lineage)
-
+    if filename:
+        # Open the file with the specified filename and read all lines
+        with open(filename) as f:
+            lines = f.readlines()
+        lines = [x.strip() for x in lines]
+        lineage = "Unknown"
+        # Iterate over each line in the file
+        for line in lines:
+            # Check if the line contains the string "Sequence Type: "
+            if "Sequence Type: " in line:
+                # If "unknown" is not in the line, extract the sequence type
+                if not "known" in line:
+                    lineage = "ST"+line.split(" ")[-1]
+        # Return the determined lineage
+        return (lineage)
+    else:
+        return ("Input file doesn't exist")
 
 def spa_data(filename):
-    spa_types = []
-    # Open the file with the specified filename and read all lines
-    with open(filename, 'r') as csvfile:
-        csvreader = csv.DictReader(csvfile, delimiter = "\t")
-        for row in csvreader:
-            # Append the value of the "Type" column from each row to the spa_types list
-            spa_types.append(row["Type"])
-    # Return the list of spa types
-    return (spa_types)
+    if filename:
+        spa_types = []
+        # Open the file with the specified filename and read all lines
+        with open(filename, 'r') as csvfile:
+            csvreader = csv.DictReader(csvfile, delimiter = "\t")
+            for row in csvreader:
+                # Append the value of the "Type" column from each row to the spa_types list
+                spa_types.append(row["Type"])
+        # Return the list of spa types
+        return (spa_types)
+    else:
+        return ("Input file doesn't exist")
 
 
 def agr_data(filename):
-    # Open the file with the specified filename and read all lines
-    with open(filename) as f:
-        lines = f.readlines()
-    lines = [x.strip() for x in lines]
-    agr_type = "Unknown"
-    # Iterate over each line in the file
-    for line in lines:
-        # Check if the line contains the string "agr typing "
-        if "agr typing " in line:
-            agr_type = "agr"+line.split(" ")[-1].replace("gp", "")
-    # Return the determined agr_type
-    return (agr_type)
+    if filename:
+        # Open the file with the specified filename and read all lines
+        with open(filename) as f:
+            lines = f.readlines()
+        lines = [x.strip() for x in lines]
+        agr_type = "Unknown"
+        # Iterate over each line in the file
+        for line in lines:
+            # Check if the line contains the string "agr typing "
+            if "agr typing " in line:
+                agr_type = "agr"+line.split(" ")[-1].replace("gp", "")
+        # Return the determined agr_type
+        return (agr_type)
+    else:
+        return ("Input file doesn't exist")
 
 
 def abricate_data(filename, value):
-    genes = []
-    # Open the file with the specified filename and read all lines
-    with open(filename) as f:
-        lines = f.readlines()
-    lines = [x.strip() for x in lines]
-    # Iterate over each line in the file
-    for line in lines:
-        if not line.startswith("#"):
-            # Append the value of the predicted ARG/VF column from each row to the genes list
-            if value == "ARG":
-                genes.append(line.split("\t")[5]+"__"+line.split("\t")[-1])
-            else:
-                genes.append(line.split("\t")[5])
-    # Remove possible redundancies and sort the list
-    genes = list(set(genes))
-    genes.sort()
-    # Return predicted genes
-    return (genes)
+    if filename:
+        genes = []
+        # Open the file with the specified filename and read all lines
+        with open(filename) as f:
+            lines = f.readlines()
+        lines = [x.strip() for x in lines]
+        # Iterate over each line in the file
+        for line in lines:
+            if not line.startswith("#"):
+                # Append the value of the predicted ARG/VF column from each row to the genes list
+                if value == "ARG":
+                    genes.append(line.split("\t")[5]+"__"+line.split("\t")[-1])
+                else:
+                    genes.append(line.split("\t")[5])
+        # Remove possible redundancies and sort the list
+        genes = list(set(genes))
+        genes.sort()
+        # Return predicted genes
+        return (genes)
+    else:
+        return ("Input file doesn't exist")
 
-
-def resfinder_data(filename):   
-    resist = []
-    # Open the file with the specified filename and read all lines
-    with open(filename, 'r') as csvfile:
-        csvreader = csv.DictReader(csvfile, delimiter = "\t")
-        for row in csvreader:
-            name = row["Mutation"].replace(" p.", "__")+"__"+row["Resistance"]
-            resist.append(name)
-    # Remove possible redundancies and sort the list
-    resist = list(set(resist))
-    resist.sort()
-    # Return predicted genes
-    return (resist)        
-
+def resfinder_data(filename):
+    if filename:
+        resist = []
+        # Open the file with the specified filename and read all lines
+        with open(filename, 'r') as csvfile:
+            csvreader = csv.DictReader(csvfile, delimiter = "\t")
+            for row in csvreader:
+                name = row["Mutation"].replace(" p.", "__")+"__"+row["Resistance"]
+                resist.append(name)
+        # Remove possible redundancies and sort the list
+        resist = list(set(resist))
+        resist.sort()
+        # Return predicted genes
+        return (resist)        
+    else:
+        return ("Input file doesn't exist")
 
 def plasmid_data(filename):
-    plasmids = []
-    # Open the file with the specified filename and read all lines
-    with open(filename, 'r') as csvfile:
-        csvreader = csv.DictReader(csvfile, delimiter = "\t")
-        for row in csvreader:
-            name = "Plasmid_"+row["sample_id"].split(":")[-1]
-            if row["relaxase_type(s)"] != "-":
-                name += "__"+row["relaxase_type(s)"]
-            if row["orit_type(s)"] != "-":
-                name += "__"+row["orit_type(s)"]
-            plasmids.append(name)
-    # Remove possible redundancies and sort the list
-    plasmids = list(set(plasmids))
-    plasmids.sort()
-    # Return predicted plasmids
-    return (plasmids)
+    if filename:
+        plasmids = []
+        # Open the file with the specified filename and read all lines
+        with open(filename, 'r') as csvfile:
+            csvreader = csv.DictReader(csvfile, delimiter = "\t")
+            for row in csvreader:
+                name = "Plasmid_"+row["sample_id"].split(":")[-1]
+                if row["relaxase_type(s)"] != "-":
+                    name += "__"+row["relaxase_type(s)"]
+                if row["orit_type(s)"] != "-":
+                    name += "__"+row["orit_type(s)"]
+                plasmids.append(name)
+        # Remove possible redundancies and sort the list
+        plasmids = list(set(plasmids))
+        plasmids.sort()
+        # Return predicted plasmids
+        return (plasmids)
+    else:
+        return ("Input file doesn't exist")
 
 
 def prophage_data(filename):
-    prophages = []
-    # Open the file with the specified filename and read all lines
-    with open(filename) as f:
-        lines = f.readlines()
-    lines = [x.strip() for x in lines]
-    for line in lines:
-        name = "Prophage_region_"+line.split("\t")[0]+"__"+line.split("\t")[1]+"__"+line.split("\t")[2]+"__"+line.split("\t")[3]
-        prophages.append(name)
-    # Remove possible redundancies and sort the list
-    prophages = list(set(prophages))
-    prophages.sort()
-    # Return predicted plasmids
-    return (prophages)
-        
-            
+    if filename:
+        prophages = []
+        # Open the file with the specified filename and read all lines
+        with open(filename) as f:
+            lines = f.readlines()
+        lines = [x.strip() for x in lines]
+        for line in lines:
+            name = "Prophage_region_"+line.split("\t")[0]+"__"+line.split("\t")[1]+"__"+line.split("\t")[2]+"__"+line.split("\t")[3]
+            prophages.append(name)
+        # Remove possible redundancies and sort the list
+        prophages = list(set(prophages))
+        prophages.sort()
+        # Return predicted plasmids
+        return (prophages)
+    else:
+        return ("Input file doesn't exist")
+    
 ###############################################################################
 
 # Load the configuration file
